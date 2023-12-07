c054ec65e9a17b69eba33001502f06e3d0c151bc
Eva Kalbfell <e.kalbfell@web.de>
2023-12-07 14:28:30 +0100

    test diary routes

--- backend/routes/diary.routes.js
+++ backend/routes/diary.routes.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const mongoose = require("mongoose")
-const Diary = require("../models/Diary.model")
+const Diary = require("../models/Diary.model");
+const isAuthenticated = require("../middleware/isAuthenticated");
 
-router.post("/diary", async (req, res, next) => {
-//   const { userID } = req.user;
-  const { userID, diaryText } = req.body;
+router.post("/diary/entries", isAuthenticated ,async (req, res, next) => {
+  const { _id: userID } = req.user;
+  const { diaryText } = req.body;
 
   try {
-    const newDiaryEntry = await Diary.create({ userID, diaryText });
+    const newDiaryEntry = (await Diary.create({ userID, diaryText }));
 
     res.status(200).json(newDiaryEntry);
   } catch (err) {
@@ -16,32 +17,41 @@ router.post("/diary", async (req, res, next) => {
   }
 });
 
-router.get("/diary/all", async (req, res, next) => {
+router.get("/diary/entries", isAuthenticated ,async (req, res, next) => {
   const { _id: userID } = req.user;
+
   try {
-    const diaryEntries = await Diary.find(userID);
+    const diaryEntries = await Diary.find({userID});
 
     if (!diaryEntries) {
       return res
-        .status(404)
-        .json({ error: "No diary entry found to this user" });
+        .json({ message: "User has no entries yet" });
     }
 
     res.status(200).json(diaryEntries);
+    
   } catch (err) {
     console.error("An error occurred:", err.message)
     next(err);
   }
 });
 
-router.get("/diary/:dateNow", async (req, res, next) => {
+router.get("/diary/entries/date/:dateNow", isAuthenticated, async (req, res, next) => {
   const { _id: userID } = req.user;
   const { dateNow } = req.params;
+  const startDate = new Date (dateNow)
+  const endDate = new Date(dateNow + "T23:59:59.999Z")
+  
   try {
     const diaryEntryByDate = await Diary.findOne({
       userID: userID,
-      date: new Date(dateNow),
+      createdAt: {
+        $gte: startDate,
+        $lt: endDate,
+      },
     });
+
+    console.log()
 
     if (!diaryEntryByDate) {
       return res.status(404).json({ error: "Diary entry not found" });
@@ -49,20 +59,21 @@ router.get("/diary/:dateNow", async (req, res, next) => {
 
     res.status(200).json(diaryEntryByDate);
   } catch (error) {
-    console.error("An error occurred:", err.message)
+    console.error("An error occurred:", error.message)
     next(error);
   }
 });
 
-router.get("/diary/:entryID", async (req, res, next) => {
+router.get("/diary/entries/:entryID", isAuthenticated ,async (req, res, next) => {
   const { entryID } = req.params;
-  
-  if( !mongoose.Types.ObjectId.isValid(entryID)) {
-    return res.status(400).json({error: "Specified id is not valid"})
-    }
+  console.log(entryID)
 
 
   try {
+    if( !mongoose.Types.ObjectId.isValid(entryID)) {
+        return res.status(400).json({error: "Specified id is not valid"})
+        }
+
     const foundDiaryEntry = await Diary.findById(entryID);
 
     if (!foundDiaryEntry) {
@@ -71,13 +82,12 @@ router.get("/diary/:entryID", async (req, res, next) => {
 
     res.status(200).json(foundDiaryEntry);
 
-  } catch (err) {
-    console.error("An error occurred:", err.message)
-    next(err);
+  } catch (error) {
+    console.error("An error occurred:", error.message)
   }
 });
 
-router.patch("/diary/:entryID", async (req, res, next) => {
+router.patch("/diary/entries/:entryID", isAuthenticated ,async (req, res, next) => {
   const { entryID } = req.params;
   const { diaryText } = req.body;
 
@@ -100,7 +110,7 @@ router.patch("/diary/:entryID", async (req, res, next) => {
   }
 });
 
-router.delete("/diary/:entryID", async (req, res, next) => {
+router.delete("/diary/entries/:entryID", isAuthenticated, async (req, res, next) => {
   const { entryID } = req.params;
 
   try {
