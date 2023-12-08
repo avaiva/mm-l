98691e70fbc523163a5a3ecaa59dc21fb9c0f0dd
Lisa Durdun <lisa.durdun@gmail.com>
2023-12-08 22:25:16 +0100

    add isAuth middleware to all profile routes, fix put so that users have to confirm their new password change AND hash new password

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -6,8 +6,9 @@ const profileRouter = require("express").Router();
 const axios = require(`axios`);
 const Diary = require("../models/Diary.model");
 const Gratitude = require("../models/Gratitude.model");
+const bcrypt = require("bcryptjs");
 
-profileRouter.get("/users/:id", async (req, res) => {
+profileRouter.get("/users/:id", isAuthenticated, async (req, res) => {
   const { id } = req.params;
 
   try {
@@ -21,20 +22,52 @@ profileRouter.get("/users/:id", async (req, res) => {
   }
 });
 
-profileRouter.put("/users/:id", async (req, res) => {
+profileRouter.put("/users/:id", isAuthenticated, async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+    const user = await User.findById(id);
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-    res.json(user);
+    if (
+      (req.body.password && !req.body.checkPassword) ||
+      (req.body.checkPassword && !req.body.password)
+    ) {
+      res.status(401).json({
+        message:
+          "Please confirm your new password by entering it into both fields.",
+      });
+      return;
+    }
+    if (req.body.password && req.body.password !== req.body.checkPassword) {
+      res
+        .status(401)
+        .json({ message: "The passwords are not matching." });
+      return;
+    }
+
+    let hashedPassword = user.password;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      hashedPassword = await bcrypt.hash(req.body.password, salt);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { ...req.body, password: hashedPassword },  
+      { new: true }
+    );
+    res.json(updatedUser);
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error from Profile route" });
   }
 });
 
-profileRouter.delete("/users/:id", async (req, res) => {
+profileRouter.delete("/users/:id", isAuthenticated, async (req, res) => {
   const { id } = req.params;
   try {
     const user = await User.findByIdAndDelete(id);
