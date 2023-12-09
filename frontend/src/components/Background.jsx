4c39ab57371807f3f1f4ebf8a9199a021b563f45
Eva Kalbfell <e.kalbfell@web.de>
2023-12-09 15:45:32 +0100

    make background div work

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -3,7 +3,19 @@
 export default function Background(props) {
 
   return (
-    <div style={{height: '100vh', width: '100vw', backgroundColor: 'yellow'}} >
+    <div
+      style={{
+        position: 'fixed',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        height: '90vh',
+        width: '90vw',
+        backgroundColor: '#F4F5F7',
+        borderRadius: '2.5rem',
+        padding: '20px',
+        boxSizing: 'border-box', // Include padding in total width and height
+      }}>
         {props.children}
     </div>
   );
