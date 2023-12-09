dbd2bc84d567c1d7b2e37dab3781c1385d01d688
Lou Gastardi <l.gastardi@hotmail.com>
2023-12-09 14:42:27 +0100

    create SaveButton Capital letters

--- frontend/src/components/ButtonSave.jsx
+++ frontend/src/components/ButtonSave.jsx
@@ -8,7 +8,7 @@ export default function ButtonSave() {
   return (
     <>
       <Button variant="secondary" onClick={handleSave} className="border-0 bg-transparent text-dark d-flex align-items-center btn-sm">
-        Save
+        SAVE
       </Button>
     </>
   );
