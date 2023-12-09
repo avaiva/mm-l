5bf82ad8d324343440f3be564852559ee74ef672
Lisa Durdun <lisa.durdun@gmail.com>
2023-12-09 12:57:05 +0100

    navigate functionality placeholder

--- frontend/src/components/BackNav.jsx
+++ frontend/src/components/BackNav.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import { BsChevronLeft } from "react-icons/bs";
+//import { useNavigate } from "react-router-dom";
 
-export default function BackNav({ onClick }) {
+export default function BackNav() {
+  //     const navigate = useNavigate();
+
+  //   const handleGoBack = () => {
+  //     navigate(-1);
+  //   };
+
   return (
     <>
       <Button
         variant="secondary"
-        onClick={onClick}
+        //onClick={handleGoBack}
         className="border-0 bg-transparent text-dark d-flex align-items-center btn-sm"
       >
         <BsChevronLeft size={14} style={{ marginRight: "6px" }} /> Go back
