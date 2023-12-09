edf6f549044dbc5967149fb1378207bd7a2b64a3
Eva Kalbfell <e.kalbfell@web.de>
2023-12-09 17:49:58 +0100

    make avatar work correctly

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -1,37 +1,41 @@
-import './Avatar.css'
+import { Link } from "react-router-dom";
+import "./Avatar.css";
 import { Container } from "react-bootstrap";
 
 export default function Avatar({ name, scale }) {
-
   const getAvatarColorClass = (letter) => {
     const lowerCaseLetter = letter.toLowerCase();
-    if ('adgjmpsvy'.includes(lowerCaseLetter)) {
-      return 'avatar-color-1';
-    } else if ('behknqtwz'.includes(lowerCaseLetter)) {
-      return 'avatar-color-2';
+    if ("adgjmpsvy".includes(lowerCaseLetter)) {
+      return "avatar-color-1";
+    } else if ("behknqtwz".includes(lowerCaseLetter)) {
+      return "avatar-color-2";
     } else {
-      return 'avatar-color-3';
+      return "avatar-color-3";
     }
   };
 
   const calculateSize = (baseSize, scaleFactor) => {
-    return `${baseSize * scaleFactor}px`
-  }
+    return `${baseSize * scaleFactor}px`;
+  };
 
   return (
     <>
-      <Container className={`my-5 d-flex justify-content-center`}>
-        <div
-          className={`avatar font-weight-bold ${getAvatarColorClass(name[0])}`}
-          style={{ 
-            height: calculateSize(150, scale), 
-            width: calculateSize(150, scale),
-            fontSize: calculateSize(78, scale),
-            fontWeight: 'bold'
-          }}
-        >
-          {name && name.length > 0 ? name[0].toUpperCase() : ""}
-        </div>
+      <Container>
+        <Link to="/account">
+          <div
+            className={`avatar font-weight-bold ${getAvatarColorClass(
+              name[0]
+            )}`}
+            style={{
+              height: calculateSize(150, scale),
+              width: calculateSize(150, scale),
+              fontSize: calculateSize(78, scale),
+              fontWeight: "bold",
+            }}
+          >
+            {name && name.length > 0 ? name[0].toUpperCase() : ""}
+          </div>
+        </Link>
       </Container>
     </>
   );
--- frontend/src/components/PageMain.jsx
+++ frontend/src/components/PageMain.jsx
@@ -1,26 +1,39 @@
 import "./PageMain.css";
 import NavBar from "./NavBar";
+import Avatar from "./Avatar";
 
 export default function PageMain() {
-    return (
-      <div style={{ position: "relative", overflow: "hidden" }}>
-        <div
-          style={{
-            position: "fixed",
-            top: "3%",
-            left: "50%",
-            transform: "translateX(-50%)",
-            height: "89vh",
-            width: "90vw",
-            backgroundColor: "#F4F5F7",
-            borderRadius: "2.5rem",
-            padding: "20px",
-            boxSizing: "border-box",
-          }}
-        ></div>
-        <div style={{ position: "fixed", bottom: "15px", left: "50%", transform: "translateX(-50%)" }}>
-          <NavBar/>
-        </div>
+  return (
+    <div style={{ position: "relative", overflow: "hidden" }}>
+      <div style={{ zIndex: "1", position: "fixed", top: "2.5em", right: "1.5em", height: "0", margin: "0"}}>
+        <Avatar name="Eva" scale="0.23" />
       </div>
-    );
-  }
+      <div
+        style={{
+          position: "fixed",
+          top: "3%",
+          left: "50%",
+          transform: "translateX(-50%)",
+          height: "89vh",
+          width: "90vw",
+          backgroundColor: "#F4F5F7",
+          borderRadius: "2.5rem",
+          padding: "20px",
+          boxSizing: "border-box",
+          zIndex: "-1",
+        }}
+      ></div>
+
+      <div
+        style={{
+          position: "fixed",
+          bottom: "15px",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <NavBar />
+      </div>
+    </div>
+  );
+}
