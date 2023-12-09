4707cfed02c899845572e8f7e7e1793d34c973c1
Eva Kalbfell <e.kalbfell@web.de>
2023-12-09 11:39:44 +0100

    set up avatar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,37 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 import InputField from "./components/InputField";
-import LandingPage  from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage";
 import SignupPage from "./pages/SignUpPage";
 import TodayPage from "./pages/TodayPage";
-import TimelinePage from "./pages/TimelinePage"
+import TimelinePage from "./pages/TimelinePage";
 import InnerSupportPage from "./pages/InnerSupportPage";
 import AccountPage from "./pages/Account";
 import EditGratitudePage from "./pages/EditGratitudePage";
 import EditDiaryPage from "./pages/EditDiaryPage";
 import NavBar from "./components/NavBar";
+import Avatar from "./components/Avatar";
 
 function App() {
-  return(
+  return (
     <>
-    <NavBar/>
-    <Routes>
-    <Route path="/" element={<LandingPage/>} />
-      <Route path="/signup" element={<SignupPage/>} />
-      <Route path="/login" element={<LandingPage/>} />
-      <Route path="/today" element={<TodayPage/>} />
-      <Route path="/edit-gratitude" element={<EditGratitudePage/>} />
-      <Route path="/edit-diary" element={<EditDiaryPage/>} />
-      <Route path="/timeline" element={<TimelinePage/>} />
-      <Route path="/inner-support" element={<InnerSupportPage/>} />
-      <Route path="/account" element={<AccountPage/>} />
-    </Routes>
-   
+      <NavBar />
+      <Avatar name="Eva" scale="0.25" />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LandingPage />} />
+        <Route path="/today" element={<TodayPage />} />
+        <Route path="/edit-gratitude" element={<EditGratitudePage />} />
+        <Route path="/edit-diary" element={<EditDiaryPage />} />
+        <Route path="/timeline" element={<TimelinePage />} />
+        <Route path="/inner-support" element={<InnerSupportPage />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import { Nav } from "react-bootstrap";
+import { Nav, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
+    <Container className="d-flex justify-content-center" style={{width: "100vw"}}>
     <Nav variant="underline">
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/timeline" activeClassName="active">
+      <Nav.Item style={{marginLeft: "0.5em", marginRight: "2em"}}>
+        <Nav.Link as={NavLink} to="/timeline" className="nav-link mml-nav">
           Timeline
         </Nav.Link>
       </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/today" activeClassName="active">
+      <Nav.Item style={{marginRight: "2em"}}>
+        <Nav.Link as={NavLink} to="/today" className="nav-link mml-nav">
           Today
         </Nav.Link>
       </Nav.Item>
-      <Nav.Item>
-        <Nav.Link as={NavLink} to="/inner-support" activeClassName="active">
-          Inner-Support
+      <Nav.Item style={{marginLeft: "0.5em"}}>
+        <Nav.Link as={NavLink} to="/inner-support" className="nav-link mml-nav">
+          MyWisdom
         </Nav.Link>
       </Nav.Item>
     </Nav>
+    </Container>
   );
 }
 
