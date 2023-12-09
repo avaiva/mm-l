37e7190ca33971840048b7b69707549acdd2e5a6
Lou Gastardi <l.gastardi@hotmail.com>
2023-12-09 13:24:25 +0100

    button changes

--- frontend/src/components/ButtonForm.jsx
+++ frontend/src/components/ButtonForm.jsx
@@ -3,6 +3,10 @@ import Button from 'react-bootstrap/Button';
 import './ButtonForm.css';
 
 export default function ButtonForm({ navigate, label, classCss }) {
+  function changeColorButton() {
+    //Add logic
+  }
+
   return (
     <div className="flex-center">
       <Button className={`btn-form ${classCss}`} size="lg" href={navigate}>
--- frontend/src/components/ButtonIconDelete.jsx
+++ frontend/src/components/ButtonIconDelete.jsx
@@ -9,8 +9,11 @@ export default function ButtonIconDelete({ navigate, label, type }) {
   };
   return (
     <Button className="btn-icon" href={navigate} type={type} onClick={handleDelete}>
-      <BsTrash />
-      {label}
+      <div className="d-flex align-items-center">
+        {/* BUTTON STYLING */}
+        <BsTrash style={{ width: '16px', height: '16px', margin: '0' }} />
+        {label}
+      </div>
     </Button>
   );
 }
