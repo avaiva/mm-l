e379614f8370ffc7f9d1943ca1dc69ebb1a77e14
Lisa Durdun <lisa.durdun@gmail.com>
2023-12-11 15:56:58 +0100

    goback and avatar fix

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,20 +1,22 @@
-import PageLanding from '../components/PageLanding';
-import ButtonForm from '../components/ButtonForm';
-import './LandingPage.css';
+import PageLanding from "../components/PageLanding";
+import ButtonForm from "../components/ButtonForm";
+import "./LandingPage.css";
 
 export default function LandingPage() {
   return (
     <>
       <PageLanding />
       <div className="landing-page-h1">
-        <h1>Be your own guru.</h1>
+        <h1>Reframe your inner voices.</h1>
       </div>
       <div className="landing-page-h4">
-        <h4>Practice gratitude and cherish life’s moments with ease. Our app is your personal companion.</h4>
+        <h4>
+          Practice gratitude and cherish the sunny moments of life with our app.
+        </h4>
       </div>
       <div className="landing-page-btn">
-        <ButtonForm navigate="/signup" label="Sign Up" classCss={'btn-grey'} />
-        <ButtonForm navigate="/login" label="Login" classCss={'btn-white'} />
+        <ButtonForm navigate="/signup" label="Sign Up" classCss={"btn-grey"} />
+        <ButtonForm navigate="/login" label="Login" classCss={"btn-white"} />
       </div>
     </>
   );
