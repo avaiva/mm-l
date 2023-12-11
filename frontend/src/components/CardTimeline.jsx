75fb1728b8924cd62f6d5716a0384c165c10d32c
Eva Kalbfell <e.kalbfell@web.de>
2023-12-11 12:04:04 +0100

    make timeline cards render

--- frontend/src/components/CardTimeline.jsx
+++ frontend/src/components/CardTimeline.jsx
@@ -9,31 +9,30 @@ export default function CardTimeline({
   return (
     <section className="timeline-wrapper">
       <div className="date">
-        <h4 className="date">05.12.2023{date}</h4>
+        <h4 className="date">{date}</h4>
       </div>
 
-      <Card className="custom-card">
-        <Card.Body>
-          <Card.Title>My gratitude</Card.Title>
-          <Card.Text>
-            {todayGratitude.text}
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content. Some quick example text to build on the
-            card title and make up the bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="custom-card">
-        <Card.Body>
-          <Card.Title>My Moment</Card.Title>
-          <Card.Text>
-            {todayDiary.text}
-            Moments Some quick example text to build on the card title and make
-            up the bulk of the card's content. Some quick example text to build
-            on the card title and make up the bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      {todayGratitude && (
+        <Card className="custom-card">
+          <Card.Body>
+            <Card.Title>My gratitude</Card.Title>
+            <Card.Text>
+              {todayGratitude}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+
+       {todayDiary && (
+        <Card className="custom-card">
+          <Card.Body>
+            <Card.Title>My Moments</Card.Title>
+            <Card.Text>
+              {todayDiary}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
     </section>
   );
 }
--- frontend/src/pages/TimelinePage.jsx
+++ frontend/src/pages/TimelinePage.jsx
@@ -1,49 +1,77 @@
-import PageMain from "../components/PageMain"
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+import PageMain from "../components/PageMain";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import CardTimeline from "../components/CardTimeline";
+import {Link } from 'react-router-dom'
 
 export default function TimelinePage() {
   const [timelineList, setTimelineList] = useState([]);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
-  console.log(timelineList[0])
+  useEffect(() => {
+    const token = localStorage.getItem("token");
 
-
-  useEffect(()=> {
-    const token = localStorage.getItem('token');
-    console.log(token)
-    axios.get("http://localhost:5005/api/timeline", {headers: {authorization: `${token}`}})
-    .then((response) => setTimelineList(response.data))
-    .catch((error) => setError(error))
-    .finally(()=> setLoading(false))
+    axios
+      .get("http://localhost:5005/api/timeline", {
+        headers: { Authorization: `${token}` },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setTimelineList(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error);
+      })
+      .finally(() => setLoading(false));
   }, []);
 
-  if(error) {
+  if (error) {
     // return <ErrorPage/>
-    return <> Error...</>
+    return <> Error...</>;
   }
 
-  if(loading) {
+  if (loading) {
     // return <LoadingSpinner/>
-    return <> Loading...</>
+    return <> Loading...</>;
   }
 
-  console.log(timelineList[0])
-
-  return(
+  return (
     <>
-    <PageMain/>
-    
-    {/* Work with divs and position it absolutely on the page. 
-    Make sure to use em to stay consistent over breakpoints. */}
-    
-    <div style={{position: "fixed", left: "50%", top: "50%", transform: "translate(-50%,-50%)"}}>
-    Hello from Timeline
-    </div>
-    <div style={{position: "fixed",top: "3em"}}>
-      that is my second div
-    </div>
+      <PageMain />
+
+      <div
+        style={{
+          position: "fixed",
+          top: "6em",
+          left: "3.5em",
+          // transform: "translate(-50%,-50%)",
+        }}
+      >
+        <h4>My life</h4>
+      </div>
+      <div style={{ position: "fixed", top: "9em", left: "3.5em", width: "80vw", textAlign: "left" }}>
+        {timelineList.length === 0 && (
+          <div>
+            <h4>Here will be all your entries <br/>
+              Start feeling gratitude and collecting precious moments of{' '} <Link to="/today">Today</Link> 
+            </h4> 
+          </div>
+        )}
+         {timelineList.length > 0 &&
+        timelineList.map((eachEntry) => (
+          <div key={eachEntry.date}>
+          {(eachEntry.gratitude.length > 0 || eachEntry.diary.length > 0) && (
+            <CardTimeline
+              date={eachEntry.date}
+              todayGratitude={(eachEntry.gratitude.length > 0) ? eachEntry.gratitude[0].text : ''}
+              todayDiary={eachEntry.diary.length > 0 ? eachEntry.diary[0].text : ''}
+            />
+          )}
+        </div>
+        ))}
+      </div>
     </>
-  ) 
+  );
 }
