d1e4512d6aa54d62080ac20a5c263f266bdac645
Lou Gastardi <l.gastardi@hotmail.com>
2023-12-12 12:05:56 +0100

    start account

--- frontend/src/components/ButtonIconLogout.jsx
+++ frontend/src/components/ButtonIconLogout.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import './ButtonIcon.css';
-import { BsTrash } from 'react-icons/bs';
+import { BsDoorClosed } from 'react-icons/bs';
 
-export default function ButtonIconDelete({ navigate, label, type, id, onClick }) {
+export default function ButtonIconLogout({ navigate, label, type, id, onClick }) {
   return (
     <Button className="btn-icon" href={navigate} type={type} onClick={onClick}>
       <div className="d-flex align-items-center">
-        <BsTrash style={{ width: '16px', height: '16px', margin: '0' }} />
+        <BsDoorClosed style={{ width: '16px', height: '16px', margin: '0' }} />
         {label}
       </div>
     </Button>
--- frontend/src/pages/TodayPage.jsx
+++ frontend/src/pages/TodayPage.jsx
@@ -1,18 +1,8 @@
 import PageMain from '../components/PageMain';
-import { useContext } from 'react';
-import { AuthContext } from '../context/auth.context';
-import { Button } from 'react-bootstrap';
 
 export default function TodayPage() {
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   return (
     <>
-      {isLoggedIn && (
-        <div>
-          <Button onClick={logOutUser}>Logout</Button>
-        </div>
-      )}
-
       {/* <div style={{ position: 'fixed', left: '50%', top: '50%', transform: 'translate(-50%,-50%)' }}>Hello from Today</div> */}
       <PageMain />
     </>
