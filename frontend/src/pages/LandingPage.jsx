e532e3959723eba14b8ce1ed701f84b3e22b5e4c
Lou Gastardi <l.gastardi@hotmail.com>
2023-12-12 17:49:58 +0100

    ani test

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,7 @@
 import PageLanding from '../components/PageLanding';
 import ButtonForm from '../components/ButtonForm';
+import Logo from '../../public/Logo.json';
+import Lottie from 'lottie-react';
 import './LandingPage.css';
 
 export default function LandingPage() {
@@ -13,6 +15,7 @@ export default function LandingPage() {
         <h4>Practice gratitude and cherish the sunny moments of life with our app.</h4>
       </div>
       <div className="landing-page-btn">
+        <Lottie animationData={Logo} />
         <ButtonForm navigate="/signup" label="Sign Up " size="lg" />
         <ButtonForm navigate="/login" label="Login " size="lg" />
       </div>
