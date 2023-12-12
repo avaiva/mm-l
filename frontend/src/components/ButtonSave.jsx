1e37ac4e84e1171af3b34dfd5b6ff0c3352b5288
Lou Gastardi <l.gastardi@hotmail.com>
2023-12-12 18:48:12 +0100

    icon buttons

--- frontend/src/components/ButtonSave.jsx
+++ frontend/src/components/ButtonSave.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import './ButtonSave.css';
 
-export default function ButtonSave({onClick}) {
+export default function ButtonSave({ onClick }) {
   const handleSubmit = () => {
     if (typeof onClick === 'function') {
       onClick();
     }
   };
   return (
-    <>
-      <Button variant="secondary" onClick={handleSubmit()} className="border-0 bg-transparent text-dark d-flex align-items-center btn-sm">
-        SAVE
+    <div className="btn-save">
+      <Button onClick={handleSubmit()}>
+        <h5>Save</h5>
       </Button>
-    </>
+    </div>
   );
 }
--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,6 +3,8 @@ import ButtonForm from '../components/ButtonForm';
 import Logo from '../../public/Logo.json';
 import Lottie from 'lottie-react';
 import './LandingPage.css';
+import ButtonSave from '../components/ButtonSave';
+import ButtonIcon from '../components/ButtonIcon';
 
 export default function LandingPage() {
   return (
@@ -15,9 +17,14 @@ export default function LandingPage() {
         <h4>Practice gratitude and cherish the sunny moments of life with our app.</h4>
       </div>
       <div className="landing-page-btn">
-        <Lottie animationData={Logo} />
+        {/* <Lottie animationData={Logo} /> */}
         <ButtonForm navigate="/signup" label="Sign Up " size="lg" />
         <ButtonForm navigate="/login" label="Login " size="lg" />
+        <ButtonSave />
+        <ButtonIcon imgSrc="../../public/delete.svg" label=" Delete account" />
+        <ButtonIcon imgSrc="../../public/edit.svg" />
+        <ButtonIcon imgSrc="../../public/logout.svg" />
+        <ButtonIcon imgSrc="../../public/goBack.svg" />
       </div>
     </>
   );
