2758b03943981e0dca31340cf8a6ac8e2abcb879
Gizem <109289446+gizemayegul@users.noreply.github.com>
2023-12-14 00:40:38 +0100

    I removed todaycontext api, added everything inside the todaypage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,18 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 // import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { TodayProviderWrapper } from "./context/today.context.jsx";
 import { AuthProviderWrapper } from "./context/auth.context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
 
   <AuthProviderWrapper>
-    <TodayProviderWrapper>
-      <Router>
-        <App />
-      </Router>
-    </TodayProviderWrapper>
+    <Router>
+      <App />
+    </Router>
   </AuthProviderWrapper>
 
   // </React.StrictMode>
